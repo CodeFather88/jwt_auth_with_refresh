3e039cc7809970e7be36446048f93e7ae51914a7
Igor-Web-Developer <bulanovigor01@gmail.com>
2024-06-01 18:04:35 +0300

    token service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,19 +9,19 @@ import {
 import { UserService } from './user.service'
 import { CreateUserDto } from './dto/createUser.dto'
 import { GetOneUserDto } from './dto/getOneUser.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DeleteUserDto } from './dto/deleteUser.dto';
 import { Role } from '@shared/enums';
 import { Roles } from '@shared/decorators';
 
 @ApiTags('User')
+@ApiBearerAuth()
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
   @ApiOperation({ summary: 'Create user' })
-    @Roles(Role.ADMIN)
-    
+  @Roles(Role.ADMIN)
   @Post()
   async createUser(@Body() dto: CreateUserDto) {
     return await this.userService.create(dto)
